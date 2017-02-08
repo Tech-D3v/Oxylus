0927937d286aed62a783b1bff4dbe0576cfdff55
William Mann <William Mann>
2017-02-08 18:38:25 +0000

    Initial Commit. Starting to setup framework.

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php
+      session_start();
+      require_once "php/cdn.php";
+    ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Oxylus Natural Ltd.</title>
+  </head>
+  <body>
+    <div class="container">
+      <header class="jumbotron">
+        <h1>Oxylus Natural Ltd.</h1>
+      </header>
+    </div>
+  </body>
+</html>
