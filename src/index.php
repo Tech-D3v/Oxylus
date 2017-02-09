63c64e37c5e7b9ffa1da2a7fa83c5e6ee0603d90
William Mann <William Mann>
2017-02-09 18:59:57 +0000

    Update #03

--- src/index.php
+++ src/index.php
@@ -11,43 +11,56 @@
     <title>Oxylus</title>
   </head>
   <body>
-    <div class="container-fluid">
-    <nav>
-      <ul>
-        <li><a>Home</a></li>
-        <li><a>Test</a></li>
-        <li><a>Test</a></li>
-      </ul>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-light bg-faded">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Oxylus</a>
+      <div class="collapse navbar-collapse" id="mainNavbar">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Shop</a>
+        </div>
+      </div>
     </nav>
-    <div id="mainCarousel" class="carousel slide" data-ride="carousel">
+    <!--Carousel -->
+    <div id="mainCarousel" class="carousel slide" data-ride="carousel" style="height: 50em; overflow: hidden;">
       <ol class="carousel-indicators">
         <li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#mainCarousel" data-slide-to="1"></li>
         <li data-target="#mainCarousel" data-slide-to="2"></li>
       </ol>
-
       <div class="carousel-inner" role="listbox">
-        <div class="item active">
-          <img src="assets/images/sample1.jpeg" alt="Sample 1" class="carousel-image">
+        <div class="carousel-item active">
+          <img class="d-block img-fluid carousel-image" src="assets/images/sample1.jpg" alt="First slide">
         </div>
-
-        <div class="item">
-          <img src="assets/images/sample2.jpeg" alt="Sample 2" class="carousel-image">
+        <div class="carousel-item">
+          <img class="d-block img-fluid carousel-image" src="assets/images/sample2.jpg" alt="Second slide">
         </div>
-
-        <div class="item">
-          <img src="assets/images/sample3.jpeg" alt="Sample 3" class="carousel-image">
+        <div class="carousel-item">
+          <img class="d-block img-fluid carousel-image" src="assets/images/sample3.jpg" alt="Third slide">
         </div>
       </div>
-      <a class="left carousel-control" href="#mainCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <a class="carousel-control-prev" href="#mainCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
-      <a class="right carousel-control" href="#mainCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <a class="carousel-control-next" href="#mainCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
-  </div>
-
+    </div>
+    <div class="container-fluid">
+      <div class="jumbotron">
+          <h1 class="display-3">Oxylus</h1>
+          <p class="lead">This is Oxylus named by Charles Sin etc. ...............</p>
+          <hr class="my-4">
+          <p>Check out our shop to purchase our products</p>
+          <p class="lead">
+            <a class="btn btn-secondary btn-lg" href="#" role="button">Shop Now!</a>
+          </p>
+      </div>
+    </div>
   </body>
 </html>
